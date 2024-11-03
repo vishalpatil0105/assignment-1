1eceae09ecd2bc8e5f4dcc13848e15e9ab5d6823
vishal patil <80039917+vishalpatil0105@users.noreply.github.com>
2024-11-03 20:27:19 +0000

    Create coupon-service-user-data.sh

---
+++ coupon-service-user-data.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+sudo yum update -y
+sudo yum install tar -y
+sudo yum install wget -y 
+
+sudo wget https://download.oracle.com/java/23/latest/jdk-23_linux-x64_bin.rpm 
+sudo rpm -i jdk-23_linux-x64_bin.rpm
+
+
+
+sudo wget https://containers-assignment-1.s3.eu-west-1.amazonaws.com/couponservice-0.0.1-SNAPSHOT.jar
+
+
+java -jar couponservice-0.0.1-SNAPSHOT.jar
